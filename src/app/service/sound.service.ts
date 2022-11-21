7a46011df5b07548f286f6d071bbd4d702e9281b
Yuri Cavalcante <yuricavalcante17@gmail.com>
2022-11-21 08:41:42 +0000

    Deploying using docs folder #6

--- src/app/service/sound.service.ts
+++ src/app/service/sound.service.ts
@@ -12,7 +12,7 @@ export class SoundService {
   playSound(){
 
     this.audio = new Audio();
-    this.audio.src = "../../assets/sounds/notification_sound.mp3";
+    this.audio.src = "./assets/sounds/notification_sound.mp3";
     this.audio.load();
     this.audio.play();
   }
