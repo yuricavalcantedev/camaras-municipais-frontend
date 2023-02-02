af29ba837cbaa1a55f4f055c0625853f66248003
Jose Ricardo Dainese <josericardodainese@gmail.com>
2023-02-02 02:37:08 -0300

    Change Body And Login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
-  frontEndUrl: 'http://localhost:4200'
+  apiUrl: 'https://floating-scrubland-72426.herokuapp.com',
+  frontEndUrl: 'https://camaras-municipais-frontend.vercel.app'
 };
